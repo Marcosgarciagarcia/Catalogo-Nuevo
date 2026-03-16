6617c609ba6cfdf3d7ab9fa16a91908648c69ab2
Marcosgarciagarcia <socramaicrag@gmail.com>
2026-03-16 12:55:11 +0100

    más correcciones

--- src/components/BookDetailModal.jsx
+++ src/components/BookDetailModal.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import './BookDetailModal. css';
+import './BookDetailModal.css';
 
 function DetailRow({ label, value }) {
   if (value == null || value === '') return null;
